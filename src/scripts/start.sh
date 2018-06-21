fc71b810463447e677c945a159f8a1d6910ec4ef
Scott Hussey <sthussey@gmail.com>
2018-06-20 20:52:42 -0500

    Add static file stub

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-uwsgi --http :8000 --file /home/scout/stem/src/python/stem/app.py --callable app --py-autoreload 1 --pp /home/scout/stem/src/python
+uwsgi --http :8000 \
+      --file /home/scout/stem/src/python/stem/app.py \
+      --callable app \
+      --py-autoreload 1 \
+      --pp /home/scout/stem/src/python \
+      --check-static /home/scout/stem/src/web
